0c98b23292d7067bee5b41ff68935d9d273c8439
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2026-02-28 17:48:32 +0000

    Small tweaks for mobile Homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,16 +13,16 @@ export default function HomePage() {
                 className="h-screen w-full bg-cover bg-center flex flex-col items-center justify-center text-white"
                 style={{ backgroundImage: `url(${HomeBg})` }}
             >
-                <div className="relative bottom-30 sm:bottom-38 md:bottom-45 flex flex-col justify-center items-center text-center px-4 sm:px-6">
-                    <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold tracking-widest">
+                <div className="relative bottom-30 sm:bottom-32 md:bottom-45 flex flex-col justify-center items-center text-center px-4 sm:px-6">
+                    <h1 className="font-extrabold tracking-widest whitespace-nowrap text-[clamp(2rem,6vw,4rem)] sm:text-[clamp(2.5rem,5vw,5rem)] md:text-7xl">
                         NEWCASTLE WILDCATS
                     </h1>
-                    <h2 className="text-2xl sm:text-2.5xl md:text-3xl font-bold tracking-widest mt-4">
+                    <h2 className="font-bold tracking-widest  whitespace-nowrap text-[clamp(1.2rem,4vw,2rem)] sm:text-[clamp(1.5rem,3.5vw,2.5rem)] md:text-3xl">
                         UNIVERSITY ICE HOCKEY CLUB
                     </h2>
                 </div>
 
-                <div className="relative -bottom-50 mt-10 flex flex-col sm:flex-row gap-4 sm:gap-15">
+                <div className="relative -bottom-40 sm:-bottom-45 md:-bottom-50 mt-10 flex flex-col sm:flex-row gap-4 sm:gap-15">
                     <Button
                         text="Join Now"
                         onClick={() => navigate('/pagenotfound')}
