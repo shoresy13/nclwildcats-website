f34fe8e437dbba624ab9e8b7408cdf44cd4b1715
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2026-02-28 17:15:52 +0000

    Update PageNotFound.jsx for mobile

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -6,21 +6,19 @@ export default function PageNotFound() {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <div className="relative top-30 flex flex-col justify-center items-center text-black">
-                <h1 className="text-7xl font-extrabold tracking-widest">
-                    WEBSITE IN DEVELOPMENT
-                </h1>
-                <h2 className="text-3xl font-bold tracking-widest mt-4">
-                    PAGE NOT FOUND, CONTACT jacoblewisshores@gmail.com
-                </h2>
+        <div className="flex flex-col justify-center items-center min-h-screen px-4 text-center">
+            <h1 className="text-4xl sm:text-7xl font-extrabold tracking-widest mt-20">
+                WEBSITE IN DEVELOPMENT
+            </h1>
+            <h2 className="text-xl sm:text-3xl font-bold tracking-widest mt-4">
+                PAGE NOT FOUND, CONTACT jacoblewisshores@gmail.com
+            </h2>
 
-                <div className="relative -bottom-50 mt-10">
-                    <Button
-                        text="Go Back"
-                        onClick={() => navigate('/')}
-                    />
-                </div>
+            <div className="mt-8">
+                <Button
+                    text="Go Back"
+                    onClick={() => navigate('/')}
+                />
             </div>
         </div>
     );
